4f349e1b65a35f8dc73e06c0f5580568b6118db4
im0qianqian <qian1335661317@qq.com>
2019-05-09 12:37:09 +0800

    README

--- src/coach.py
+++ src/coach.py
@@ -146,8 +146,11 @@ class Coach(object):
             # 我在这里偷偷的拷贝一份没有人知道吧
             shutil.copyfile(os.path.join(self.args.checkpoint_folder, self.args.train_folder_file),
                             os.path.join(self.args.checkpoint_folder, 'checkpoint_{}_update.pth.tar'.format(idx)))
+
             # 这一步是删除旧版本的 best.pth.tar （其实也可以用下一步的 move 覆盖掉，然而谷歌云盘的历史版本很困扰惹）
-            os.remove(os.path.join(self.args.checkpoint_folder, self.args.best_folder_file))
+            # 好像即使删掉原文件也会记录历史版本，那还是删掉吧
+            # os.remove(os.path.join(self.args.checkpoint_folder, self.args.best_folder_file))
+
             # 复制文件 train_folder_file 到 best_folder_file
             shutil.move(os.path.join(self.args.checkpoint_folder, self.args.train_folder_file),
                         os.path.join(self.args.checkpoint_folder, self.args.best_folder_file))
